ebce2d6d3b52f54b3ed9bfb84b159e4afc7ab809
徐宁 <xuning@192.168.1.6>
2018-08-23 16:36:19 +0800

    ..

---
+++ R/SVR.R
@@ -0,0 +1,7 @@
+svr<-function(y){
+  require(e1071)
+  ds=data.frame(y=y,t=1:length(y))
+  svr.model<-svm(y~t,data=ds,type="eps-regression",kernel="radial")
+  MAPE<-mean(abs(ds$y-svr.model$fitted)/ds$y)
+  return(MAPE)
+}
