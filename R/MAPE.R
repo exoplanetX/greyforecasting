2c9a227294305a4d0a99819115984a04046dbb94
exoplanetX <nuaa_xuning@163.com>
2018-11-20 21:39:56 +0800

    update ghg2 data to 2016

--- R/MAPE.R
+++ R/MAPE.R
@@ -2,3 +2,6 @@ mape<- function(original,simulation){
   error=mean(abs(original-simulation)/original)
   return(error)
 }
+ape<-function(original,simulation){
+  abs(original-simulation)/original
+}
--- R/SVR.R
+++ R/SVR.R
@@ -3,5 +3,6 @@ svr<-function(y){
   ds=data.frame(y=y,t=1:length(y))
   svr.model<-svm(y~t,data=ds,type="eps-regression",kernel="radial")
   MAPE<-mean(abs(ds$y-svr.model$fitted)/ds$y)
-  return(MAPE)
+  cat(MAPE)
+  return(svr.model)
 }
